23ce4e15a605650885b7043d2f98bf6268fc5cf5
Harshini-Gunasekar <146466317+Harshini-Gunasekar@users.noreply.github.com>
2024-12-28 23:02:32 +0530

    Add files via upload

---
+++ binary operators.js
@@ -0,0 +1,54 @@
+
+//arithmetic operators
+/*let a=9;
+let b=3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+*/
+
+//comparison operators
+/*let x=10;
+let y=20;
+console.log(x>y);
+console.log(x<y);
+console.log(x==y); //comparing only values
+console.log(x===y); //comapring both value and type
+console.log(x!=y);
+console.log(x>=20);
+console.log(x<=20)
+*/
+
+//logical operators
+/*let a=true;
+let b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+*/
+
+//bitwise operators
+/*let a=5; //bianry:0101
+let b=3; //bimary:0011
+
+console.log(a&b);
+console.log(a|b);
+console.log(a^b);
+console.log(~a);
+console.log(a<<1);
+console.log(a>>1);
+
+//assignment operators
+let x=10;
+x+=5;
+console.log(x);
+x-=2;
+console.log(x);
+x*=3;
+console.log(x);
+x/=3;
+console.log(x);*/
+
+
