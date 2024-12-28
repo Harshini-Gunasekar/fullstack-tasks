e3e33ab91f143e67d84298fff0f39b3a33f5a04e
Harshini-Gunasekar <146466317+Harshini-Gunasekar@users.noreply.github.com>
2024-12-28 21:46:10 +0530

    Add files via upload

---
+++ task day 2.js
@@ -0,0 +1,53 @@
+let date;
+let month;
+let year;
+function check(date,month,year)
+{
+    if(month===2)
+    {
+        if((year % 4 ===0 && year %100 !==0) ||(year%400===0))
+                {
+                    
+                    if(date===29)
+                    {
+                        console.log("valid leap year date");
+                    }
+                    else if(date<=28)
+                    {
+                        console.log("valid leap year date");
+                    }
+                    else{
+                        console.log("invalid leap year");
+                    }
+                }
+    }
+    else if(month===1 ||month===3 ||month===5 ||month===7 ||month===8 ||month===10 ||month===12)
+    {
+        if(date===31 || date<31)
+        {
+            console.log("valid");
+        }
+        else{
+            console.log("invalid");
+        }
+    }
+    else if(month===4 ||month===6 ||month===9 ||month===11)
+     {
+        if(date<=30)
+        {
+            console.log("valid");
+        }
+        else{
+            console.log("invalid")
+        }
+     }
+     else{
+        console.log("wrong");
+     }
+                    
+
+}
+
+check(31,4,2024);
+check(28,2,2000);
+check(7,9,2003);
